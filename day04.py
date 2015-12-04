18f300e3ba386ffd0d65953ff5c9a9924ea8695d
Aidan Feay <aidanlfeay@gmail.com>
2015-12-04 12:18:56 -0500

    day04 timed

--- day04.py
+++ day04.py
@@ -1,10 +1,15 @@
 import hashlib
 import re
+import time
 
+timer = time.time()
 hashVal = hashlib.md5('')
 numVal = 0
-pattern = re.compile('^0{5}')
+pattern = re.compile('^0{6}')
 while not pattern.match(hashVal.hexdigest()):
   numVal += 1
   hashVal = hashlib.md5('yzbqklnj' + str(numVal))
 print numVal, hashVal.hexdigest()
+print time.time() - timer
+
+# this is slow as sin
